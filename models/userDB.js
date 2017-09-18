707b25c689e5da75e17dd5e2d51c0dd24846e99a
haamseongho <haams704@gmail.com>
2017-09-18 15:44:45 +0900

    resolving conflic

--- models/userDB.js
+++ models/userDB.js
@@ -60,7 +60,7 @@ var userSchema = new Schema({
 
 });
 
-var User = mongoose.model('User', userSchema);
+
 
 userSchema.methods.getSpotData = function (lat, lon, callback) {
     this.model("User").collection.insert({lat: lat, lon: lon}, callback);
@@ -95,5 +95,5 @@ userSchema.methods.getLTID = function (tel, callback) {
     this.model("User").collection.findOne({tel: tel}, callback);
     // 전화번호 일치시 callback 주기 --> LTID 가져올 예정.
 };
-
+var User = mongoose.model('User', userSchema);
 module.exports = User;
