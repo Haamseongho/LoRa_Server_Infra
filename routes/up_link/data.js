c00234df018f0a9b8df37012bce14ec3228360ab
root <root@ip-172-31-11-196.ap-northeast-2.compute.internal>
2017-11-01 12:22:42 +0900

    push alarm succeeded

--- routes/up_link/data.js
+++ routes/up_link/data.js
@@ -8,14 +8,32 @@ require('body-parser-xml')(bodyParser);
 var db = mongoose.connection;
 
 var app = express();
-var fcmPush = require("./push"); // FCM PUSH
+var request = require("request");
 
 app.set(bodyParser.json());
 app.set(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.xml());
 
 
-router.post("/", function (req, res, next) {
+var Fcm = require("fcm-node");
+var serverKey = "AAAAkNmJaXU:APA91bE0QYvaSD-yXIR0BfYETtD9WRfve90kgAn9LP5LZtEOW0n5hwOVOs2oP9bc5tcHD9czK_oUl3lKYC0AVuPxj432LQJry7Q4kGOfrEv1G5KUmuy_WgF5Yev4hDYuUvX1B9t9hnIq";
+
+var fcm = new Fcm(serverKey);
+
+var message = {
+        "to":"cKSgvj1FDN4:APA91bHeBUZ4sWsHGYrslyFU2lowTt4115fxXvQkaIAoTdo8C0sMZQ0j4lVfkUqtEXksHo5IPlKhQfS8vSftCmcQ60xm8ZIARTxhL9_DXIpWPbmoOR6J_szIU1odcRrjL3dJBv5iu5jl",
+        "notification" : {
+            "body" : "디바이스 사용자의 맥박이 이상 수치로 측정되었습니다. 빠른 조치가 필요합니다.",
+            "title": "위험 상황! 노인 분께서 위험합니다!"
+        },
+        "data":{
+            "name":"디바이스 착용자 위험 상황 알림",
+            "body":"맥박이 이상 수치로 측정되었습니다. 빠르게 조치해야 합>니다!"
+        }
+    };
+
+
+router.post("/data", function (req, res, next) {
     var data = new Array();  // sr 정보 뽑기 위함
     var data2 = new Array(); // LTID 체크 하기 위함 
 
@@ -44,20 +62,39 @@ router.post("/", function (req, res, next) {
     console.log(lat + "위도");
     console.log(lon + '경도');
     console.log(pulse + '맥박');
-
+/*
     if (pulse < 60 || pulse > 140) {
         fcmPush(router, function (err) {
             if (err) console.log(new Error("푸쉬 알림 전송 실패"));
             else console.log("푸쉬 알림 전송 성공");
         });
     }
-
+*/
+    if(pulse < 90){
+	var json_obj = {"msg":"hi","aa":["aaa","aaaa"]};
+	request.post({
+	   url : "http://52.79.83.51:2721/push/message",
+	   body : json_obj,
+	   json : true
+        },function(error,response,body){
+	  console.log(body);
+	  res.end();
+	});
+    }
 
     dynamic.save(function (error, data) {
-        if (error) return res.json("error saved user's Data");
-        return res.send('send well');
+        if (error) console.log("error saved user's Data");
+        else console.log('data saved well');
     });
 
+});
+
+router.post("/push/message",function(req,res,next){
+    console.log(req.url);
+    fcm.send(message,function(err,response){
+	if(err) console.log("메시지 전송 실패");
+	else console.log("메시지 전송 성공");
+    });
 });
 
 module.exports = router;
